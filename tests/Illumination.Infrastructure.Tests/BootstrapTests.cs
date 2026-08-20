2d565e6ac99af7a65aaa17c35852d01f1a1dcc48
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-20 05:07:49 +0200

    test: account for Deck learning activity profile entity

--- tests/Illumination.Infrastructure.Tests/BootstrapTests.cs
+++ tests/Illumination.Infrastructure.Tests/BootstrapTests.cs
@@ -15,7 +15,7 @@ public class BootstrapTests
 
         using var context = new IlluminationDbContext(options);
 
-        Assert.Equal(17, context.Model.GetEntityTypes().Count());
+        Assert.Equal(18, context.Model.GetEntityTypes().Count());
     }
 
     [Fact]
