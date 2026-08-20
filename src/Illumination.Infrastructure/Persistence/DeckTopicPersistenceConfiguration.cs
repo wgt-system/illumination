09b88cfc3e878ee88596409bff0c2b82ac7916a9
Wilhelm Altemeier <55293156+Wiiii90@users.noreply.github.com>
2026-08-20 03:22:32 +0200

    feat: configure queryable Deck topic labels

---
+++ src/Illumination.Infrastructure/Persistence/DeckTopicPersistenceConfiguration.cs
@@ -0,0 +1,26 @@
+using Illumination.Domain.Decks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Illumination.Infrastructure.Persistence;
+
+internal static class DeckTopicPersistenceConfiguration
+{
+    public static void Configure(ModelBuilder modelBuilder)
+    {
+        var labels = modelBuilder.Entity<DeckTopicLabelRecord>();
+        labels.ToTable("DeckTopicLabels");
+        labels.HasKey(x => new { x.DeckId, x.Label });
+        labels.Property(x => x.DeckId).HasColumnName("DeckId");
+        labels.Property(x => x.Label)
+            .HasColumnName("Label")
+            .HasMaxLength(Deck.MaximumTopicLabelLength)
+            .UseCollation("NOCASE")
+            .IsRequired();
+
+        modelBuilder.Entity<DeckRecord>()
+            .HasMany(x => x.TopicLabels)
+            .WithOne(x => x.Deck)
+            .HasForeignKey(x => x.DeckId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
